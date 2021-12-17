34e70c58f596098c01378f8fd9fa460d25e25252
✨小透明・宸✨ <1272742391@qq.com>
2021-12-17 21:19:29 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ pyinstaller \
     --noconsole \
     --clean \
     --key $key \
-    --log-level WARNING \
+    --log-level WARN \
     --add-data "assets${pyiSeparator}assets" \
     --add-data "font/SourceHanSerifSC-Medium.otf${pyiSeparator}font" \
     --add-data "scriptfiles${pyiSeparator}scriptfiles" \
